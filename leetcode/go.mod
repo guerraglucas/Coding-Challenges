70fe20476108a2b84391af5f332d10dc8fbb15f1
Lucas Guerra <guerraglucas@MacBook-Air-de-Lucas.local>
2023-05-15 20:15:27 -0300

    adding greatestCommonDivisorString challenge

---
+++ leetcode/go.mod
@@ -0,0 +1,3 @@
+module github.com/guerraglucas/Coding-Challenges/leetcode
+
+go 1.20
---
+++ leetcode/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	arrays "github.com/guerraglucas/Coding-Challenges/leetcode/arrays"
+)
+
+func main() {
+	fmt.Println(arrays.GcdOfStrings("AAAAAAAAA", "AAACCC"))
+}
