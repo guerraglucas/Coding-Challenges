98788ebf10679415a3f151aec1eed94a6acb9693
Lucas Guerra <lucasguerra34@gmail.com>
2023-05-24 16:09:07 -0300

    stack removeStarsFromString challenge

--- leetcode/stack/removingStarsFromString.go
+++ leetcode/stack/removingStarsFromString.go
@@ -40,5 +40,15 @@
 package stacks
 
 func removeStars(s string) string {
-
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if s[i] == '*' {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return string(stack)
 }
